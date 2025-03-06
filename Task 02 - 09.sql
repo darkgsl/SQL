b74de4046eb4d549fa4736da5af9d71cd29cbbb9
darkgsl <darkgsl@gmail.com>
2025-03-06 12:23:59 +0300

    add Task 2 - 9

--- Task 02 - 09.sql
+++ Task 02 - 09.sql
@@ -5,8 +5,135 @@ Show the average quantity of books each reader currently has
 taken.
 */
 
+
+WITH subscriber_book_counts AS (
+    SELECT
+        sb_subscriber,
+		subscr.s_name,
+        COUNT(sb_book) AS book_count
+    FROM
+        subscriptions
+	JOIN
+		subscribers AS subscr ON s_id = sb_subscriber
+    GROUP BY
+        sb_subscriber
+),
+aggregated_data AS (
+    SELECT
+        MAX(book_count) AS max_books,
+        AVG(book_count) AS avg_books
+    FROM
+        subscriber_book_counts
+)
 SELECT
-	sb_subscriber, AVG(count(sb_book)) as average
+    s.sb_subscriber,
+    s.s_name,
+    s.book_count,
+    a.max_books,
+    a.avg_books
+FROM
+    subscriber_book_counts s,
+    aggregated_data a;
+    
+    
+    
+
+#здесь все работает, но неправильно считает срднее
+SELECT 
+	sb_subscriber, s_name, book_count, average
+FROM (
+	SELECT
+		sb_subscriber, book_count_internal as book_count, AVG(book_count_internal) as average
+	FROM
+		# subscriptions
+        (
+        SELECT
+        		sb_subscriber, count(sb_book) as book_count_internal
+		FROM
+			subscriptions
+		GROUP BY sb_subscriber
+		) as subquey2
+        
+GROUP BY sb_subscriber
+) AS subquery
+	JOIN
+		subscribers AS s ON s_id = sb_subscriber
+GROUP BY sb_subscriber;
+
+#, s_name
+SELECT 
+    sb_subscriber,
+    s.s_name,
+   # COUNT(sub.sb_book) AS book_count,
+    AVG(sub.book_count) AS average
+FROM (
+    SELECT
+        sb_subscriber,
+        COUNT(sb_book) AS book_count
+    FROM
+        subscriptions
+    GROUP BY
+        sb_subscriber
+) AS sub
+JOIN 
+    subscribers AS s ON s.s_id = sub.sb_subscriber
+GROUP BY 
+    sb_subscriber
+    
+    , s.s_name;
+    
+    #------
+#-----
+
+
+
+SELECT 
+    s.sb_subscriber,
+    s.s_name,
+    COUNT(sub.sb_book) AS "всего книг",
+    AVG(sub.book_count) AS average
+FROM (
+    SELECT
+        sb_subscriber,
+        COUNT(sb_book) AS book_count
+    FROM
+        subscriptions
+    GROUP BY
+        sb_subscriber, sb_book
+) AS sub
+JOIN 
+    subscribers AS s ON s.s_id = sub.sb_subscriber
+GROUP BY 
+    s.sb_subscriber, s.s_name;
+    
+    
+    #-----------
+    SELECT 
+    s.s_id AS sb_subscriber,
+    s.s_name,
+    COUNT(sub.sb_book) AS "всего книг",
+    AVG(sub.book_count) AS average
+FROM (
+    SELECT
+        sb_subscriber,
+        sb_book,
+        COUNT(sb_book) AS book_count
+    FROM
+        subscriptions
+    GROUP BY
+        sb_subscriber, sb_book
+) AS sub
+JOIN 
+    subscribers AS s ON s.s_id = sub.sb_subscriber
+GROUP BY 
+    s.s_id, s.s_name;
+    #=====
+    
+    
+№----------
+
+SELECT
+	sb_subscriber, count(sb_book) as average
 FROM
 	subscriptions
 GROUP BY sb_subscriber
